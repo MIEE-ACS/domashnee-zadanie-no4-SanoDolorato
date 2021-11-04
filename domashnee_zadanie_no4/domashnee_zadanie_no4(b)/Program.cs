b4456a647d8da19a55433dddb7bcb60f091f4da1
SanoDolorato <90412284+SanoDolorato@users.noreply.github.com>
2021-11-04 13:28:52 +0300

    Создали другой проект в этом же решении
    Двумерный массив

---
+++ domashnee_zadanie_no4/domashnee_zadanie_no4(b)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace domashnee_zadanie_no4_b_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
