c7501e187cc9f6cb08bdd36bc9493f18cc6c4638
SanoDolorato <90412284+SanoDolorato@users.noreply.github.com>
2021-11-04 14:26:56 +0300

    Продолжение
    Рефакторинг, выполнение задания

--- domashnee_zadanie_no4/domashnee_zadanie_no4(b)/Program.cs
+++ domashnee_zadanie_no4/domashnee_zadanie_no4(b)/Program.cs
@@ -11,25 +11,53 @@ namespace domashnee_zadanie_no4_b_
 
             while (n < 1)
             {
-                Console.WriteLine("Введите количество элементов массива (n>0):");
+                Console.WriteLine("Введите стороны двумерного массива (n>0):");
                 n = Convert.ToInt32(Console.ReadLine());
                 if (n < 1)
                 {
                     Console.WriteLine("Данные введены некорректно");
                 }
             }
-            int[,] myMatrix = new int[n,n];
+            int[,] myMatrix = new int[n, n];
+            completionarray(n, myMatrix);
+            conclusionarray(n, myMatrix);
+            Console.WriteLine("Произведения элементов в тех строках, которые не содержат отрицательных элементов");
+
+            for (int i = 0; i < n; i++)
+            {
+                int k = 1;
+                int flag = 1;//
+                for (int j = 0; j < n; j++)
+                {
+
+                    if (myMatrix[i, j] < 0)
+                        flag = 0;
+                    k = k * myMatrix[i, j];
+                }
+                if (flag == 1)
+                {
+                    Console.WriteLine($"Произведение элементов в {i + 1} строке: {k}");
+                }
+            }
+        }
+
+        private static void completionarray(int n, int[,] myMatrix)
+        {
             Random random = new Random();
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
-                    myMatrix[i,j] = random.Next(-15, 15);
+                    myMatrix[i, j] = random.Next(-4, 15);
+        }
+
+        private static void conclusionarray(int n, int[,] myMatrix)
+        {
+            Console.WriteLine("Вывод матрицы");
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                     Console.Write("{0}\t", myMatrix[i, j]);
                 Console.WriteLine();
             }
-
         }
     }
 }
