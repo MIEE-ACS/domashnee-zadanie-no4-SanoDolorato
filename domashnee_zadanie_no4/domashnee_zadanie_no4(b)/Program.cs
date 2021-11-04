92963cf9b5bf45e874a9f8ae62554c87d5a6b139
SanoDolorato <90412284+SanoDolorato@users.noreply.github.com>
2021-11-04 13:40:49 +0300

    Начало второго проекта
    Двумерная матрица

--- domashnee_zadanie_no4/domashnee_zadanie_no4(b)/Program.cs
+++ domashnee_zadanie_no4/domashnee_zadanie_no4(b)/Program.cs
@@ -6,7 +6,30 @@ namespace domashnee_zadanie_no4_b_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 0;
+            Console.WriteLine("Двумерный массив");
+
+            while (n < 1)
+            {
+                Console.WriteLine("Введите количество элементов массива (n>0):");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n < 1)
+                {
+                    Console.WriteLine("Данные введены некорректно");
+                }
+            }
+            int[,] myMatrix = new int[n,n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    myMatrix[i,j] = random.Next(-15, 15);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write("{0}\t", myMatrix[i, j]);
+                Console.WriteLine();
+            }
+
         }
     }
 }
